a5e2d283b6e149b98fe8859315432485070c7637
anstg <anshertg@gmail.com>
2018-03-01 00:06:29 +0530

    created AddNodes function yet to be completed

--- slinkedlist.cpp
+++ slinkedlist.cpp
@@ -9,35 +9,40 @@ struct node{
 void AddNodes()
 {
 	int n,loop,value;
+	NODE *temp;
+	temp=(struct node*)malloc(sizeof(struct node));
 	printf("How many nodes do you want to add");
 	scanf("%d",&n);  // ASKING USER THE LENGTH OF THE LIST
-
 
 	printf("\n Enter value at node %d= ",loop+1);
 	scanf("%d",value); // VALUE AT FIRST NODE
 
-	if(head==NULL){
+		if(n=1){ //condition when user wants to enter only 1 node
 
-			head=current; // LINKING HEAD TO CURRENT NODE!
-  		current->data=
-  	}
+		  		head->link=current->link; // LINKING HEAD TO CURRENT NODE!
+  				current->data=value;
+					current->link=NULL;
+					printf("Single Node added=> %2d\n",current->data);
+  		}
 
 	else{
-				for(loop=1;loop<n;loop++)
-		  {
+					for(loop=1;loop<n;loop++){
 
-		printf("\n Enter value at node %d= ",loop+1);
-		scanf("%d",value);
 
-	}
+								printf("\n Enter value at node %d= ",loop+1);
+								scanf("%d",value);
+								temp->data=value;
+								current->link
+
+						}
 	}
 }
 
 int main()
 {
 
-	NODE head=NULL;
-	NODE current=NULL;
+	NODE *head=NULL; //note: *
+	NODE *current=NULL; //note: *
 
 	head=(struct node*)malloc(sizeof(struct node));
 	current=(struct node*)malloc(sizeof(struct node));
