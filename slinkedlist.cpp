5951227b8654aa16db4f77fcae552597de55fbb7
anstg <anshertg@gmail.com>
2018-02-28 23:12:47 +0530

    added some code

--- slinkedlist.cpp
+++ slinkedlist.cpp
@@ -1,37 +1,49 @@
 #include<stdio.h>
-#include<cstdlib.h>
+#include<stdlib.h>
 typedef struct node NODE;
 struct node{
 	int data;
 	struct node *next;
-};
+}; // CREATING A STRUCTURE NODE
+
+void AddNodes()
+{
+	int n,loop,value;
+	printf("How many nodes do you want to add");
+	scanf("%d",&n);  // ASKING USER THE LENGTH OF THE LIST
+
+
+	printf("\n Enter value at node %d= ",loop+1);
+	scanf("%d",value); // VALUE AT FIRST NODE
+
+	if(head==NULL){
+
+			head=current; // LINKING HEAD TO CURRENT NODE!
+  		current->data=
+  	}
+
+	else{
+				for(loop=1;loop<n;loop++)
+		  {
+
+		printf("\n Enter value at node %d= ",loop+1);
+		scanf("%d",value);
+
+	}
+	}
+}
 
 int main()
-{   
-	int n,loop,value;
+{
+
 	NODE head=NULL;
 	NODE current=NULL;
-	
+
 	head=(struct node*)malloc(sizeof(struct node));
 	current=(struct node*)malloc(sizeof(struct node));
 
-	printf("How many nodes do you want to add");
-	scanf("%d",&n);
-	
-	printf("\n Enter value at node %d= ",loop+1);
-	scanf("%d",value);
-	
-	if(n>1)	
-	{
-	for(loop=1;loop<n;loop++)
-	{
-		printf("\n Enter value at node %d= ",loop+1);
-		scanf("%d",value);
-			
-	}
-	}
-	
+	AddNodes();
 
-	
+
+
 }
-
