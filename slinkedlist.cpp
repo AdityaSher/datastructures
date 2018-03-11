390c602e32e9fb9d4b535aff71f5433d7a70ee43
anstg <anshertg@gmail.com>
2018-03-12 02:00:12 +0530

    Completed add nodes and print nodes function

--- slinkedlist.cpp
+++ slinkedlist.cpp
@@ -1,54 +1,68 @@
+//ctrl shift h opens git-plus menu
 #include<stdio.h>
 #include<stdlib.h>
-typedef struct node NODE;
+
 struct node{
 	int data;
-	struct node *next;
+	struct node* link;
 }; // CREATING A STRUCTURE NODE
 
-void AddNodes()
-{
-	int n,loop,value;
-	NODE *temp;
-	temp=(struct node*)malloc(sizeof(struct node));
-	printf("How many nodes do you want to add");
-	scanf("%d",&n);  // ASKING USER THE LENGTH OF THE LIST
+typedef struct node NODE; // defininf struct node as one word "NODE"
 
-	printf("\n Enter value at node %d= ",loop+1);
-	scanf("%d",value); // VALUE AT FIRST NODE
+NODE* head=NULL; //note: *warning dont declare it inside main without passing it to other functions!
+NODE* current=NULL; //note: *warning dont declare it inside main without passing it to other functions!
+NODE* temp=NULL;//note: *warning dont declare it inside main without passing it to other functions!
 
-		if(n=1){ //condition when user wants to enter only 1 node
+int AddNodes(); // function for adding nodes
+int PrintList(); // function to print the singly linked list
 
-		  		head->link=current->link; // LINKING HEAD TO CURRENT NODE!
-  				current->data=value;
-					current->link=NULL;
-					printf("Single Node added=> %2d\n",current->data);
-  		}
-
-	else{
-					for(loop=1;loop<n;loop++){
-
-
-								printf("\n Enter value at node %d= ",loop+1);
-								scanf("%d",value);
-								temp->data=value;
-								current->link
-
-						}
-	}
-}
 
 int main()
 {
-
-	NODE *head=NULL; //note: *
-	NODE *current=NULL; //note: *
-
-	head=(struct node*)malloc(sizeof(struct node));
-	current=(struct node*)malloc(sizeof(struct node));
+	current=(NODE*)malloc(sizeof(NODE));
 
 	AddNodes();
+	PrintList();
 
+}
+
+int AddNodes()
+{
+	int n,loop,value;
+
+
+	printf("How many nodes do you want to add ");
+	scanf("%d",&n);  // ASKING USER THE LENGTH OF THE LIST
+
+		for(loop=0;loop<n;loop++){
+				temp=(NODE*)malloc(sizeof(NODE));
+				temp->link=NULL;
+				printf("\n Enter value at node %d= ",loop+1);
+				scanf("%d",&temp->data);
+
+				if(head==NULL)
+					{
+						head=temp;
+					}
+				else{
+
+					current->link=temp;
+				}
+
+				current=temp;
+			}//end of for
+
+}
+//INT ADD NODES ENDS
+
+int PrintList(){
+
+						current=head;
+						 while(current!=NULL){ //condition such that current will reach last block go to the null pointer and then stop
+							 		printf("%2d" ,current->data );
+									current=current->link;
+
+						 }
 
 
 }
