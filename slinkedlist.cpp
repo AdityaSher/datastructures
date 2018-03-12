f14c4b4b9ef7bc685b33a77f65262b85a93bed57
anstg <anshertg@gmail.com>
2018-03-12 23:21:09 +0530

    added InsertNode function

--- slinkedlist.cpp
+++ slinkedlist.cpp
@@ -7,7 +7,7 @@ struct node{
 	struct node* link;
 }; // CREATING A STRUCTURE NODE
 
-typedef struct node NODE; // defininf struct node as one word "NODE"
+typedef struct node NODE; // defining struct node as one word "NODE"
 
 NODE* head=NULL; //note: *warning dont declare it inside main without passing it to other functions!
 // note: *DONT ALLOCATE HEAD ITS JUST A NORMAL POINTER*
@@ -17,14 +17,20 @@ NODE* temp=NULL;//note: *warning dont declare it inside main without passing it 
 int AddNodes(); // function for adding nodes
 int PrintList(); // function to print the singly linked list
 int CountNodes(); //function to count nodes
-
+int InsertNode();// function to insert node at selected position
+int DeleteNode();//function to delete node from selected position
 int main()
 {
-	int choice;
+	int choice=0;
 	current=(NODE*)malloc(sizeof(NODE));//allocation of traversing variable must be done only once
-	printf("Choose what to do: \n");
-	printf("1. Add nodes\n 2. Create nodes 3. Count nodes\n 9. Exit");
 
+
+while(choice!=9)
+{
+
+	printf("Choose what to do: \n");
+	printf("1. Add nodes\n2. Create nodes \n3. Count nodes\n4. Insert Node \n9. Exit \n");
+	scanf("%d",&choice);
 	switch (choice) {
 		case 1: AddNodes();
 						break;
@@ -32,14 +38,19 @@ int main()
 						break;
 		case 3: CountNodes();
 						break;
-		case 9: exit();
-						break
+		case 4: InsertNode();
+						break;
+		case 5: DeleteNode();
+						break;
+		case 9: exit;
+						break;
 		default: printf("Enter Proper Input");
 						 break;
 
 	}
 
 
+}
 
 }
 
@@ -94,3 +105,66 @@ int CountNodes(){
 
 			printf("Number of nodes is= %d \n",count);
 }// INT CountNodes ENDS
+
+
+int InsertNode(){
+		int position,count;
+		temp=NULL; // avoiding temp pointing to garbage value
+		temp=(NODE*)malloc(sizeof(NODE)); // allocating temp
+		count=CountNodes(); // because we need size of list
+		current=head;
+		printf("Please enter the value you want at new node\n");
+		scanf("%d",&temp->data); // storing data directly in temp
+		printf("At what position do you want to add a node?");
+		printf("\n1. First \n2. Middle \n3. Last \n4.Custom");
+		scanf("%d",&position);
+		switch(position){
+			case 1:
+						{	temp->link=NULL;
+							current=head; //saving curren first node in current
+							head=temp;// changing first node to temp
+							temp->link=current;//linking new fiest node to old first node
+							break;
+						}
+			case 2:
+						{
+							temp->link=NULL;
+							current=head;
+							for(int x=0;x<=count/2;x++){
+										current=current->link;
+							}
+							current->link=temp;
+							temp->link=NULL;
+							break;
+						}
+
+			case 3:
+						{
+							temp->link=NULL;
+							current=head;
+							while(current->link!=NULL){
+								current=current->link;
+							}
+							current->link=temp;
+							temp->link=NULL;
+							break;
+						}
+			case 4:
+						{	int custom;
+							temp->link=NULL;
+							current=head;
+							printf("At what location do you want to insert node?\n");\
+							scanf("%d",&custom);
+							for(int x=0;x<custom;x++){
+										current=current->link;
+							}
+							current->link=temp;
+							temp->link=NULL;
+							break;
+						}
+		}
+
+		printf("Lets check the list now\n");
+		PrintList();
+
+}//InsertNode ENDS
